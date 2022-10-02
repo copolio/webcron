8efa35a9db3f1c67be9f4e23314bf10d4ae614d4
Luke <rlwjd1504@gmail.com>
2022-10-02 14:41:09 +0900

    Init Project

---
+++ src/main/kotlin/com/copolio/apiquartz/ApiQuartzApplication.kt
@@ -0,0 +1,11 @@
+package com.copolio.apiquartz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiQuartzApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApiQuartzApplication>(*args)
+}
---
+++ src/test/kotlin/com/copolio/apiquartz/ApiQuartzApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.copolio.apiquartz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiQuartzApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
