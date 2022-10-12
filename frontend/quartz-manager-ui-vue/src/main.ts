a7f050822892419863f1299eb8379f3537923915
Luke <rlwjd1504@gmail.com>
2022-10-12 23:13:12 +0900

    feat: add ant-design-vue

--- frontend/quartz-manager-ui-vue/src/main.ts
+++ frontend/quartz-manager-ui-vue/src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+const app = createApp(App);
+app.use(Antd).mount("#app");
