ba1f8bc63b4b22760aa59c4c565ef1f4c314aa3b
Luke <rlwjd1504@gmail.com>
2022-10-12 23:27:08 +0900

    feat: add pinia & openapigenerator

--- frontend/quartz-manager-ui-vue/src/main.ts
+++ frontend/quartz-manager-ui-vue/src/main.ts
@@ -1,6 +1,11 @@
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 const app = createApp(App);
-app.use(Antd).mount("#app");
+const pinia = createPinia();
+
+app.use(Antd);
+app.use(pinia);
+app.mount("#app");
