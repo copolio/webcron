85bd57f119830015395f6bdb2545ca0e31be409e
Luke <rlwjd1504@gmail.com>
2022-10-13 00:06:17 +0900

    chore: added cors configuration for vue dev server

---
+++ backend/src/main/kotlin/com/copolio/quartzmanager/config/WebMvcConfig.kt
@@ -0,0 +1,21 @@
+package com.copolio.quartzmanager.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://127.0.0.1:3001/")
+            .allowedMethods(
+                HttpMethod.GET.name,
+                HttpMethod.POST.name,
+                HttpMethod.PUT.name,
+                HttpMethod.PATCH.name,
+                HttpMethod.DELETE.name,
+            )
+    }
+}
