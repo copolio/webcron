1eb2efbae7205fb01b49f83f2a83aa1d4423c74d
Luke <rlwjd1504@gmail.com>
2022-10-24 13:17:54 +0900

    feat: added typescript support for antdv

--- frontend/quartz-manager-ui-vue/vite.config.ts
+++ frontend/quartz-manager-ui-vue/vite.config.ts
@@ -1,13 +1,20 @@
 import vue from "@vitejs/plugin-vue";
-import {defineConfig} from "vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
+
+// your plugin installation
+Components({
+  resolvers: [AntDesignVueResolver()],
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    build: {
-        outDir: "../../backend/src/main/resources/static",
-    },
-    server: {
-        port: 3000,
-    },
+  plugins: [vue()],
+  build: {
+    outDir: "../../backend/src/main/resources/static",
+  },
+  server: {
+    port: 3000,
+  },
 });
