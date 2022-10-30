69fb6d134a0142e2e8ea5175f5c380f82833d3ad
Luke <rlwjd1504@gmail.com>
2022-10-30 22:55:54 +0900

    fix: vite plugin setting for unplugin-vue-components

--- frontend/quartz-manager-ui-vue/vite.config.ts
+++ frontend/quartz-manager-ui-vue/vite.config.ts
@@ -3,14 +3,14 @@ import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 import Components from "unplugin-vue-components/vite";
 import { defineConfig } from "vite";
 
-// your plugin installation
-Components({
-  resolvers: [AntDesignVueResolver()],
-});
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
   build: {
     outDir: "../../backend/src/main/resources/static",
   },
