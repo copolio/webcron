b8881e2ad7b43e2aab75eaa37a390a845f5e3a46
Luke <rlwjd1504@gmail.com>
2023-08-03 23:08:57 +0900

    feat: init openfeign client module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,5 @@ rootProject.name = "webcron"
 include("applications:webcron")
 include("applications:webcron-ui")
 include("core:spring-yaml-importer")
+include("clients:webcron-client")
 include("domains:db-quartz")
